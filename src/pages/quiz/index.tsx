65e1617d189e24d5a65454303ec8691daa8eb54f
radziszop <radek.krochmall@gmail.com>
2023-09-30 21:48:02 +0200

    styles fix

--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -1,8 +1,10 @@
 import { AnimatePresence } from "framer-motion";
 import { useState } from "react";
+import Nav from "~/components/Nav";
 import { Page1 } from "~/components/QuizPages/Page1";
 import { Page2 } from "~/components/QuizPages/Page2";
 import { Page3 } from "~/components/QuizPages/Page3";
+import { Page4 } from "~/components/QuizPages/Page4";
 export default function Step1() {
   const [page, setPage] = useState(0);
   const nextPage = (number = 1) => {
@@ -10,17 +12,23 @@ export default function Step1() {
   };
 
   return (
-    <div className="flex h-screen w-screen items-center justify-center p-2">
-      <div className="flex gap-2">
-        <AnimatePresence mode="popLayout">
-          {page === 0 && <Page1 nextPage={nextPage} />}
-        </AnimatePresence>
-        <AnimatePresence mode="popLayout">
-          {page === 1 && <Page2 nextPage={nextPage} />}
-        </AnimatePresence>
-        <AnimatePresence>
-          {page === 2 && <Page3 nextPage={nextPage} />}
-        </AnimatePresence>
+    <div className="h-screen">
+      <Nav />
+      <div className=" mt-[-6rem] flex h-screen items-center justify-center p-2">
+        <div className="flex flex-col gap-2">
+          <AnimatePresence mode="popLayout">
+            {page === 0 && <Page1 nextPage={nextPage} />}
+          </AnimatePresence>
+          <AnimatePresence mode="popLayout">
+            {page === 1 && <Page2 nextPage={nextPage} />}
+          </AnimatePresence>
+          <AnimatePresence>
+            {page === 2 && <Page3 nextPage={nextPage} />}
+          </AnimatePresence>
+          <AnimatePresence>
+            {page === 3 && <Page4 nextPage={nextPage} />}
+          </AnimatePresence>
+        </div>
       </div>
     </div>
   );
