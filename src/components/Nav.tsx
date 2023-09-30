d84ce1d63e6c182dcb41b75088e4e081a2c76357
Dawid <dawid.drobny@zs6sobieski.edu.pl>
2023-09-30 14:43:40 +0200

    fix shadcn

---
+++ src/components/Nav.tsx
@@ -0,0 +1,13 @@
+import { Input } from "@/components/ui/input";
+
+
+export default function Nav(){
+    return (
+       <div className="flex">
+        <h1 className="text-3xl"> TEST </h1>
+        <p>FAQ</p>
+        <p>About Us</p>
+        <Input />
+       </div>
+    )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { type AppType } from "next/app";
-
+import { Inter } from "next/font/google";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+const inter = Inter({ subsets: ['latin'] })
+
+// const ibm_serif = IBM_Plex_Serif({
+//   weight: '400',
+//   subsets: ['latin']
+// }) 
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
 };
 
 export default api.withTRPC(MyApp);
