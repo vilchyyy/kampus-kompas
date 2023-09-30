9dc05aaf357fd0d30bc4586de35b7c4fb8d72297
Dawid <dawid.drobny@zs6sobieski.edu.pl>
2023-09-30 19:52:23 +0200

    nowy index

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -17,7 +17,7 @@ export default function Nav({className, logoFont}: NavProps){
        <div onClick={(e) => {
               e.preventDefault();
               router.push("/").catch((err) => console.log(err));
-       }} className="hover:cursor-pointer flex w-full h-20 gap-6 px-4 justify-between items-center max-w-7xl">
+       }} className="hover:cursor-pointer flex w-full h-20 gap-6 px-4 justify-between items-center sm:max-w-screen-2xl">
         <div className="flex gap-6 justify-center items-center mx-4">
             <Image src="compass.svg" className="h-12 w-12 min-w-max -mr-3 " width={50} height={50} alt="logo" />
             <p className="font-bold mr-4 sm:block hidden">KampusKompas</p>
